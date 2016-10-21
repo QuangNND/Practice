622f3a454504c720efd329f06708c408443488f9
QuangNND <duyquang12985@gmail.com>
2016-10-21 15:19:51 +0700

    findMax feature

--- Practice/src/NumberList.java
+++ Practice/src/NumberList.java
@@ -1,4 +1,11 @@
 
 public class NumberList {
-
+	public int findMax(int[] nums){
+		int max = 0;
+		for(int i = 0; i < nums.length(); i++){
+			if(nums[i] > max){
+				max = num[i]			}
+		{
+		return max;
+	}
 }
